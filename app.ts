10fa2f0ca63382ec5e8020384511e3be76a4e3bc
Soumik Sen <mastersoumik2003@gmail.com>
2025-04-02 00:18:44 +0530

    Updated

--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
-/* var a = 12;
+var a = 12;
 var b = 13;
 
 function abcd(){
     let x = 12;
     let y = 22;
-} */
+}
 
 // primitive types and reference types
 
@@ -14,22 +14,22 @@ function abcd(){
 
 // we can directly copy primitive types but not reference types
 
-/* var a = 12; // number
+var a = 12; // number
 var b = a;
 console.log(b);
 
 var c = [1,34,23,45,56];
 var d = c; // reference type
 
-console.log(d); */
+console.log(d);
 
 //let arr1 = [1,2,3,4,5,{name:"harsh"},{name:12}];//wrong
 
 //explicitly specify
-/* let nums : number[] = [1,2,3,4]; */
+let nums : number[] = [1,2,3,4];
 
 //Tuples
-/* let arr: [string,number] = ["harsh",22];
+let arr: [string,number] = ["harsh",22];
 
 //Enums
 enum UserRoles{
@@ -43,34 +43,34 @@ enum StatusCodes{
     NOTFOUND = "not found status code 404",
 }
 
-console.log(StatusCodes.NOTFOUND); */
+console.log(StatusCodes.NOTFOUND);
 
-/* let a : number;
+let a : number;
 a=12;
 
 function test():never{
     while(true){}
 }
 
-test(); */
+test();
 // never means it will never return anything
 
-/* let b: number | boolean | string;
+let b: number | boolean | string;
 b=12;
 b=true;
-b="harsh"; */
+b="harsh";
 
-/* function test(a:number, b:string):void{
+function test(a:number, b:string):void{
     console.log(a+b);
     return; // void means it will not return anything
 }
 
-test(12,"harsh"); */
+test(12,"harsh");
 
 // Interfaces
 // interface is used to define the structure of an object
 
-/* interface User{
+interface User{
     name:string,
     email:string,
     password:string,
@@ -96,20 +96,20 @@ function getDataOfAdmin(obj : Admin)
     console.log(obj.name+" "+obj.email+" "+obj.password+" "+obj.gender+" "+obj.admin);
 }
 
-getDataOfAdmin({name:"harsh",email:"harsh2025@gmail.com",password:"pass123",gender:"male",admin:true}); */
+getDataOfAdmin({name:"harsh",email:"harsh2025@gmail.com",password:"pass123",gender:"male",admin:true});
 
-/* type arg = string | null;
+type arg = string | null;
 
 function abcd(obj : arg)
 {
   console.log(obj)
 }
 
-abcd("harsh"); */
+abcd("harsh");
 
 // Intersection types
 
-/* type User = {
+type User = {
   name: string;
   email: string;
 };
@@ -126,29 +126,29 @@ const admin: Admin = {
   },
 };
 
-admin.getDetails({ name: "harsh", email: "harsh2025@gmail.com" }); */
+admin.getDetails({ name: "harsh", email: "harsh2025@gmail.com" });
 
 // Classes
 
-/* class Device {
+class Device {
   name = "lg";
   price = 12000;
   category = "digital";
 }
 
 let d1 = new Device();
-let d2 = new Device(); */
+let d2 = new Device();
 
-/* class Bottle
+class Bottle
 {
     radius = 120;
     price = 100;
     color = "white";
 }
 
-let b1 = new Bottle(); */
+let b1 = new Bottle();
 
-/* class BottleMaker
+class BottleMaker
 {
     constructor(public name:string, public price:number){}
 }
@@ -160,10 +160,10 @@ let b1 = new BottleMaker("Milton",1200); */
     constructor(public name:string,public isHandsome:boolean,public age:number=0){} //setting default value to age
 } 
 
-new HumanMaker("Harsh",true); */
+new HumanMaker("Harsh",true);
 
 
-/* class Music
+class Music
 {
     constructor(public name:string,public artist:string,public thumbnail:string="somethumbnail.jpg",public length:number, public free:boolean)
     {
@@ -176,12 +176,12 @@ new HumanMaker("Harsh",true); */
 
 let m1 = new Music("Chal Chaiyya Chaiyya","Harsh Sharma","",1200,false); // setting default value to thumbnail
 
-let m2 = new Music("Timeless","Starboy","poster.png",2000,true); */
+let m2 = new Music("Timeless","Starboy","poster.png",2000,true);
 
 
 // this keyword
 
-/* class abcd
+class abcd
 {
     name = "harsh";
     changeSomeStuff()
@@ -193,10 +193,10 @@ let m2 = new Music("Timeless","Starboy","poster.png",2000,true); */
     {
         console.log("Hey")
     }
-} */
+}
 
 
-/* class BottleMaker
+class BottleMaker
 {
    constructor(public name:String)
    {
@@ -204,7 +204,7 @@ let m2 = new Music("Timeless","Starboy","poster.png",2000,true); */
    }
 }
 
-let  b1 = new BottleMaker("milton"); */
+let  b1 = new BottleMaker("milton");
 
 
 
@@ -213,7 +213,7 @@ let  b1 = new BottleMaker("milton"); */
 
 
 //public
-/* class BottleMaker
+class BottleMaker
 {
     constructor(public name:string){}
 
@@ -232,12 +232,12 @@ class MetalBottleMaker extends BottleMaker
 }
 
 let b2 = new MetalBottleMaker("Milton");
-b2.getValue();  */
+b2.getValue(); 
 
 
 //private 
 
-/* class BottleMaker
+class BottleMaker
 {
     constructor(private name:string){}
 
@@ -257,11 +257,11 @@ class MetalBottleMaker extends BottleMaker
 
 let b2 = new MetalBottleMaker("Milton");
 b2.getValue(); 
- */
+
 
 
 // protected
-/* class BottleMaker
+class BottleMaker
 {
     protected name = "Harsh";
 }
@@ -277,12 +277,12 @@ class MetalBottleMaker extends BottleMaker
 }
 
 let b1 = new MetalBottleMaker();
-b1.changeName(); */
+b1.changeName();
 
 
 // Getters and Setters
 
-/* class User
+class User
 {
     constructor(public name:string,public age:number){}
 
@@ -300,12 +300,12 @@ b1.changeName(); */
 let u1 = new User("Harsh",22);
 console.log(u1.getName()); //Harsh
 u1.setName("Shanu");
-console.log(u1.getName()); //Shanu */
+console.log(u1.getName()); //Shanu
 
 
 // Static Properties and Methods
 
-/* class Hero
+class Hero
 {
     static version = 1.0;
 
@@ -316,13 +316,13 @@ console.log(u1.getName()); //Shanu */
 }
 
 console.log(Hero.version);
-console.log(Hero.getRandomNumber()); // static properties and methods can be accessed without creating an instance of the class */
+console.log(Hero.getRandomNumber()); // static properties and methods can be accessed without creating an instance of the class
 
 // Abstract Classes and Methods
 
 // Abstract Classes and Methods
 
-/* abstract class CookingEssentials {
+abstract class CookingEssentials {
     constructor(protected gas: number, public gasName: string) {}
 
     // Abstract method that must be implemented by subclasses
@@ -355,17 +355,17 @@ console.log(chicken.checkGasLevel());
 
 const cake = new Cake(3, "CNG");
 cake.cook();
-console.log(cake.checkGasLevel()); */
+console.log(cake.checkGasLevel());
 
 
 //Functions 
 
-/* function login() : string
+function login() : string
 {
    return "Heyy";
-} */
+}
 
-/* function abcd(name: string, callback: (value: string) => void): void 
+function abcd(name: string, callback: (value: string) => void): void 
 {
     console.log(name);  // Log the name first --> Harsh
     callback(`Hello, ${name}!`);  // Call the callback with a meaningful message
@@ -373,40 +373,40 @@ console.log(cake.checkGasLevel()); */
 
 abcd("Harsh", (value: string) => {
     console.log(value); // Will print: "Hello, Harsh!"
-}); */
+});
 
 
 //Optional Parameters
 
-/* function abcdef(name:string,age:number,gender?:string){
+function abcdef(name:string,age:number,gender?:string){
 
 }
 
 abcdef("Harsh",22,"male"); // all parameters can be given
 abcdef("Harsh",22); // not all parameters are required
 // Optional parameters should be at the end of the function signature
-// Optional parameters can be undefined or not passed at all */
+// Optional parameters can be undefined or not passed at all
 
 
 // Default Parameters
-/* function abcdef(name:string,age:number,gender:string="not to be disclosed"){
+function abcdef(name:string,age:number,gender:string="not to be disclosed"){
     console.log(name+" "+age+" "+gender)
 }
 
 abcdef("Harsh",22);
-abcdef("Harsh",22,"male"); // default parameters can be overridden */
+abcdef("Harsh",22,"male"); // default parameters can be overridden
 
 
 //Rest Parameters
 
-/* function abcd(...arr : number[]) // rest operator can be used to pass any number of arguments
+function abcd(...arr : number[]) // rest operator can be used to pass any number of arguments
 {
     console.log(arr); 
 }
 
-abcd(1,2,3,4,5,6,7,8); */
+abcd(1,2,3,4,5,6,7,8);
 
-/* let arr2: string[] = []; // ✅ Initialized to an empty array
+let arr2: string[] = []; // ✅ Initialized to an empty array
 
 function getTogether(...friends: string[]) {
     console.log(friends);  // Logs the array of friends
@@ -417,12 +417,12 @@ function getTogether(...friends: string[]) {
 
 getTogether("Aman", "Suman", "Abhay");
 console.log(arr2); // Output: ["Aman", "Suman", "Abhay"]
- */
+
 
 
 // Function Overloading
 
-/* function abcd(a : string) : void; // Typescript function signature
+function abcd(a : string) : void; // Typescript function signature
 function abcd(a : string, b : number) : number;
 
 function abcd(a : any, b? : any)
@@ -443,12 +443,12 @@ function abcd(a : any, b? : any)
 
 abcd("Hey");
 abcd("hey",12); // returns 123
-//abcd(12); // gives error because it is not defined in the function signature */
+//abcd(12); // gives error because it is not defined in the function signature
 
 
 // Generics
 // deciding while using a function that in what way it should treat the arguments
-/* function logger <T> (a : T) //T for template class although you can use anything in place of T
+function logger <T> (a : T) //T for template class although you can use anything in place of T
 {
    console.log(a);
 }
@@ -457,20 +457,20 @@ logger("heyy");
 logger(12);
 logger(true);
 logger(undefined);
-logger(null); */
+logger(null);
 
 
-/* function abcd<H>(a : H, b : string, c : number)
+function abcd<H>(a : H, b : string, c : number)
 {
    console.log(a +" "+b+" "+c);
 }
 
-abcd<String>("Halua","Puri",4); */
+abcd<String>("Halua","Puri",4);
 
 
 // Generic Interfaces
 
-/* interface Halua<T> // generic interface
+interface Halua<T> // generic interface
 {
     name : string,
     age : number,
@@ -482,12 +482,12 @@ function abcd(obj : Halua<String>) // generic function
    console.log(obj.name+" "+obj.age+" "+obj.key);
 }
 
-abcd({name:"food",age:25,key:"abcdefghij"}); */
+abcd({name:"food",age:25,key:"abcdefghij"});
 
 
 // Generic Classes
 
-/* class BottleMaker<T>
+class BottleMaker<T>
 {
     constructor(public key : T)
     {
@@ -499,18 +499,18 @@ let b1 = new BottleMaker<String>("Hello");
 console.log(b1);
 let b2 = new BottleMaker<number>(12);
 console.log(b2); 
-// generic classes can be used to create objects of different types */
+// generic classes can be used to create objects of different types
 
 
 // Special case of generics
-/* function abcd<T>(a:T,b:T): T // here T is used as a type for both a and b but return type cannot be set as T hence giving error while returning string 
+function abcd<T>(a:T,b:T): T // here T is used as a type for both a and b but return type cannot be set as T hence giving error while returning string 
 {
    //return "hey"; // error because here it is a string literal and T can be of any type
     
-}*/
+}
 
 //correct format
-/* function abcd<T>(a:T,b:T):T
+function abcd<T>(a:T,b:T):T
 {
   // return "hey" as T; // type assertion is used to tell typescript that this is of type T
 
@@ -518,7 +518,7 @@ console.log(b2);
   return <T>"hey"; // type assertion is used to tell typescript that this is of type T
 }
 
-abcd<string>("hey","hello"); */
+abcd<string>("hey","hello");
 
 
 
@@ -526,7 +526,7 @@ abcd<string>("hey","hello"); */
 
 // importing modules from other files in Typescript
 
-/* import { addPayment } from "./moduleTest";
+import { addPayment } from "./moduleTest";
 
 addPayment(1234);
 
@@ -534,13 +534,13 @@ import Payment from "./moduleTest";
 
 let p1 = new Payment(12);
 let p2 = new Payment(1234);
-console.log(p1,p2);  */
+console.log(p1,p2); 
 
 
 // Type assertion
 // We can use type assertion to tell typescript that this is of this type
 
-/* let a : any = 12;
+let a : any = 12;
 
 (a as number).valueOf(); // type assertion is used to tell typescript that this is of type number
 
@@ -552,14 +552,14 @@ console.log(p1,p2);  */
 // Type casting
 
 let n = Number("12");
-console.log(n); //12 */
+console.log(n); //12
 
 
 
 // Type Guards
 // Type guards are used to check the type of a variable at runtime
 
-/* function abcd(arg : string | number)
+function abcd(arg : string | number)
 {
    if(typeof arg === "number")
    {
@@ -575,12 +575,12 @@ console.log(n); //12 */
 
 console.log(abcd(12));
 console.log(abcd("hello"));
-//abcd(true); // gives error because it is not string or number */
+//abcd(true); // gives error because it is not string or number
 
 
 // Another way of type guards is using instanceof operator
 
-/* class TvRemote{
+class TvRemote{
     switchTvOff(){
         console.log("Switching off TV");
 
@@ -606,6 +606,6 @@ function SwitchOffFinal(device : TvRemote | CarRemote){
 }
 
 SwitchOffFinal(tv); // Switching off TV
-SwitchOffFinal(car); // Switching off Car */
+SwitchOffFinal(car); // Switching off Car
 
 
